8173296c869f6544e1bc84292055f7c6b1696665
Sai <sairamvijay9876@gmail.com>
2025-09-14 18:49:38 +0530

    Accounts: add UserProfile, signals, migrations

---
+++ backend/.history/accounts/models_20250914170057.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.conf import settings
+
+User = get_user_model()   # safer than importing User directly
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = (('student','Student'), ('recruiter','Recruiter'))
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,...)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
+
+    def __str__(self):
+        return f"{self.user.username} ({self.role})"
