a8c1fe181f9432338765fab2cb4260642ff6f793
Sai <sairamvijay9876@gmail.com>
2025-10-25 16:32:51 +0530

    Fix: remove tracked files and apply .gitignore

--- .history/static/login_20250915100133.js
+++
@@ -1,5 +0,0 @@
-// after login
-localStorage.setItem('token', token);
-const profile = await fetch('/api/accounts/profile/', { headers: { 'Authorization':'Bearer '+token } }).then(r=>r.json());
-if (profile.role==='recruiter') window.location='/resumes/recruiter_dashboard/';
-else window.location='/resumes/candidate_dashboard/';
